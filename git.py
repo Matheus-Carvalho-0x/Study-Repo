f1b7e1e152a59d663ac1a01b8c322ebaec9891d9
Matheus Carvalho <matheus.contato.1902@gmail.com>
2026-01-19 05:30:53 -0300

    Git sheetcode add

---
+++ git.py
@@ -0,0 +1,27 @@
+# git init
+
+# git --global (or not) config user.name ""
+# git --global (or not) config user.email ""
+# git config --list
+# git remote add origin (ssh/https)
+# git clone (ssh/https)
+# git remote -v
+
+# git status
+# git add . (or file name)
+# git restore --staged (file name)
+# git commit -m "(message here)"
+# git push
+
+# git log
+# git log --oneline
+# git log --oneline --all
+# git log --oneline --all --graph
+# git switch --detach (#hash)
+# git switch master (or main)
+# git revert (#hash)
+
+# git branch (name)
+# git branch
+# git switch (name)
+# git branch -D (name)
